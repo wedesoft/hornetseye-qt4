e3b8ee86929cd30c25ab055567b9a960394e93de
Jan Wedekind <jan.wedekind@roke.co.uk>
2025-06-30 11:11:44 +0100

    Update link to homepage

--- config.rb
+++ config.rb
@@ -19,4 +19,4 @@ DESCRIPTION = %q{This Ruby extension provides graphical output of images for Qt4
 LICENSE = 'GPL-3+'
 AUTHOR = %q{Jan Wedekind}
 EMAIL = %q{jan@wedesoft.de}
-HOMEPAGE = %q{http://wedesoft.github.com/hornetseye-qt4/}
+HOMEPAGE = %q{http://wedesoft.github.io/hornetseye-qt4/}
