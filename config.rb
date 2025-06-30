386c6bde351f304149f1e935fb2730914c7c5d96
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-06-30 12:33:59 +0100

    Update version number

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 require 'rake'
 
 PKG_NAME = 'hornetseye-qt4'
-PKG_VERSION = '0.3.4'
+PKG_VERSION = '0.3.5'
 CFG = RbConfig::CONFIG
 CXX = ENV[ 'CXX' ] || 'g++'
 RB_FILES = ['config.rb'] + FileList[ 'lib/**/*.rb' ]
